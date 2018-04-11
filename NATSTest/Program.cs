614a495fdc5755a538c35c7f2ac7c6049a7f57f1
JuanEstradaUY <jestrada@velocitypartners.net>
2018-04-11 11:19:00 -0300

    inital commit

---
+++ NATSTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using NATS.Client;
+using Newtonsoft.Json;
+
+namespace NATSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cf = new ConnectionFactory();
+
+            var c = cf.CreateConnection("nats://events.service.owf-dev:4222");
+
+            void EventHandler(object sender, MsgHandlerEventArgs e)
+            {
+                Console.WriteLine(e.Message.Subject);
+                Console.WriteLine(format_json(Encoding.UTF8.GetString(e.Message.Data)));
+            }
+
+            var eventName = args != null && args.Any() ? args[0] : "deluxe.metadata-ingest.*";
+            
+            var sAsync = c.SubscribeAsync(eventName);
+            sAsync.MessageHandler += EventHandler;
+            sAsync.Start();
+        }
+
+        private static string format_json(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+    }
+}
