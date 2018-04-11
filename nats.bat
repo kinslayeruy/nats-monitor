398d017149037a3d89f1b259963d17f58ed50037
JuanEstradaUY <jestrada@velocitypartners.net>
2018-04-11 11:20:30 -0300

    script

--- nats.bat
+++ nats.bat
@@ -1,10 +1,10 @@
 @echo off
 IF "%1"=="-log" (
 tailblazer nats.log
-echo NATS Monitoring... Logging to nats.log file.  Use Ctrl-C to exit
+echo NATS Monitoring... %2 Logging to nats.log file.  Use Ctrl-C to exit
 dotnet c:\tools\NATS\NATSTest.dll %2 > nats.log
 ) ELSE (
 cls
-echo NATS Monitoring...  Use Ctrl-C to exit
+echo NATS Monitoring... %1  Use Ctrl-C to exit
 dotnet c:\tools\NATS\NATSTest.dll %1
 )
