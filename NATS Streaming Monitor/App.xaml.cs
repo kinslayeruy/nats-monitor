9cffaf093ec0f98f2639a72ec23313bd4c7542c2
JuanEstradaUY <jestrada@velocitypartners.net>
2018-06-08 14:02:20 -0300

    added mutlithreadding

---
+++ NATS Streaming Monitor/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NATS_Streaming_Monitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ NATS Streaming Monitor/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NATS_Streaming_Monitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
