20da4cd2c15e8a234ae89b921303bc082ac74b14
Karel Tamayo <Karel.Tamayo@endava.com>
2018-09-11 11:57:27 -0300

    Update the module updater to create the target directories of don't exist and deploy modules

--- update.bat
+++ update.bat
@@ -1,2 +1,6 @@
 git pull
-copy /V /Y DeluxePSModule\* %USERPROFILE%\Documents\WindowsPowerShell\Modules\Deluxe
+mkdir %USERPROFILE%\Documents\WindowsPowerShell\Modules\Deluxe
+copy /V /Y DeluxePSModule\*.* %USERPROFILE%\Documents\WindowsPowerShell\Modules\Deluxe
+
+mkdir %USERPROFILE%\Documents\WindowsPowerShell\Modules\DockerModule
+copy /V /Y DockerModule\*.* %USERPROFILE%\Documents\WindowsPowerShell\Modules\DockerModule
