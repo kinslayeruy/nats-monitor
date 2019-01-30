4b7b365091f15b1a3dbbdd319c3306e2c5562660
Juan Estrada <Juan.Estrada@endava.com>
2019-01-30 11:31:13 -0300

    removed ps module

--- DeluxePSModule/Deluxe.psm1
+++
@@ -1,8 +0,0 @@
-﻿. $PSScriptRoot\Utils.ps1
-. $PSScriptRoot\Classes.ps1
-. $PSScriptRoot\Compare.ps1
-. $PSScriptRoot\Send-Preparser.ps1
-. $PSScriptRoot\Send-Rosetta.ps1
-. $PSScriptRoot\Compare-RvP.ps1
-. $PSScriptRoot\Send-Ingest.ps1
-. $PSScriptRoot\Get-Versions.ps1
